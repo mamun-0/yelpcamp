0cacdb4a90b09aa8e8a8416916dcf13ab26b9bc7
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-02-09 11:04:40 +0600

    CRUD operation of campgrounds and fix middleware for body parsing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 mongoose
@@ -15,7 +17,45 @@ mongoose
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 //middleware
-app.set(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+// routes
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campground/index', { campgrounds });
+});
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campground/new');
+});
+app.post('/campground', async (req, res) => {
+  const { campground } = req.body;
+  const newCampground = new Campground({ ...campground });
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
+});
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campground/edit', { campground });
+});
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const { campground } = req.body;
+  const updatedCampground = await Campground.findByIdAndUpdate(id, {
+    ...campground,
+  });
+  res.redirect(`/campgrounds/${id}`);
+});
+app.get('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campground/show', { campground });
+});
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
+});
 
 app.all('*', (req, res) => {
   res.send('404 page not found');

1c2ac7dd833c69e052fc5a336757d5b91639bf78
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-07-12 08:31:22 +0600

    fix image requirement into joischema

--- JoiSchema/joiSchema.js
+++ JoiSchema/joiSchema.js
@@ -3,7 +3,7 @@ module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
     price: Joi.number().min(0).required(),
-    // image: Joi.string().required(),
+    image: Joi.string(),
     description: Joi.string().required(),
     location: Joi.string().required(),
   }).required(),
